07e2b14cadd9d37e16a5b13ddd862226105b08fc
segiraldom <segiraldom@unal.edu.co>
2025-06-08 18:41:15 -0500

    Copia de la funcion ya existente imprimir_tabla

---
+++ impresion.py
@@ -0,0 +1,31 @@
+import  numpy as np
+from tabulate import tabulate
+
+# Función para imprimir tabla detallada
+def imprimir_tabla(poblacion, fitness, factibles, pesos, iteracion, operadores=None,):
+    total_fitness = np.sum(fitness)
+    probabilidades = fitness / total_fitness if total_fitness > 0 else np.zeros(len(fitness))
+    prob_acumulada = np.cumsum(probabilidades)
+    
+    headers = ["Individuo", "Cromosoma", "Z (Fitness)", "Factible", "Peso", "Probabilidad", "Prob. Acumulada"]
+    tabla = []
+    
+    for i in range(len(poblacion)):
+        cromosoma_lista = poblacion[i].tolist()
+        peso = np.sum(poblacion[i] * pesos)
+        tabla.append([
+            i+1,
+            cromosoma_lista,
+            fitness[i],
+            "Sí" if factibles[i] else "No",
+            peso,
+            f"{probabilidades[i]:.4f}" if total_fitness > 0 else "0.0000",
+            f"{prob_acumulada[i]:.4f}" if total_fitness > 0 else "0.0000"
+        ])
+    
+    print(f"\n--- Generación {iteracion} ---")
+    if operadores:
+        print(f"Operadores usados: Selección={operadores['seleccion'].name}, Cruce={operadores['cruce'].name}, Mutación={operadores['mutacion'].name}")
+    print(f"Fitness total: {total_fitness:.2f}")
+    print(f"Individuos factibles: {sum(factibles)}/{len(factibles)}")
+    print(tabulate(tabla, headers=headers, tablefmt="grid", floatfmt=".4f"))
